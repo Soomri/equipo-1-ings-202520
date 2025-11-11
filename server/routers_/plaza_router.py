9a0bcb898b7201940af8fd61ee4fa54fd36fee57
Soomri <sofia.mejiar@upb.edu.co>
2025-11-11 16:20:37 -0500

    Feat: View information on all marketplaces

--- server/routers_/plaza_router.py
+++ server/routers_/plaza_router.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from schemas.plaza_schema import PlazaDetalle
 from services.plaza_service import obtener_plaza_por_id
+from models import PlazaMercado
 
 router = APIRouter(prefix="/plazas", tags=["Plazas"])
 
@@ -12,3 +13,15 @@ def obtener_plaza(plaza_id: int, db: Session = Depends(get_db)):
     if not plaza:
         raise HTTPException(status_code=404, detail="Plaza no encontrada")
     return plaza
+
+@router.get("/", summary="Obtener todas las plazas de mercado")
+def obtener_plazas(db: Session = Depends(get_db)):
+    """
+    Retorna una lista con la información de todas las plazas de mercado.
+    """
+    plazas = db.query(PlazaMercado).all()
+
+    if not plazas:
+        raise HTTPException(status_code=404, detail="No hay plazas registradas")
+
+    return plazas
